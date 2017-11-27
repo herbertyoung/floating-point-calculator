63b069c6b68a9024ab9fc59ed220d94c9272050d
Harbor Young <yhb241@163.com>
2017-11-27 16:36:36 +0800

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    FloatingPointCalculator: './src/FloatingPointCalculator.js'
+  },
+  output: {
+    filename: '[name].min.js',
+    path: __dirname + '/build'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      loader: 'babel-loader'
+    }]
+  }
+};
